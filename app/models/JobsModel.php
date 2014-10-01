f14761f8ddc7a454fa10581d3df0f6ddb6570223
Robert de Vries <r.devries@robbytu.net>
2014-10-01 18:04:52 +0200

    + Added model classes for Employees, Jobs and Locations

---
+++ app/models/JobsModel.php
@@ -0,0 +1,19 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * JobsModel.php
+ * Hanzecontact
+ *
+ * This file contains the Jobs model definition
+ *
+ * @author     R. de Vries <r.devries@robbytu.net>
+ * @version    1.0.0
+ * @date       01/10/14
+ * @copyright  2014 RobbytuProjects
+ * @license    MIT License
+ */
+
+class JobsModel extends MvcBaseModel {
+
+}
