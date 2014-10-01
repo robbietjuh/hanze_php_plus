0308cf37cec30b6ad0cf5407f8774950c59f8ab4
Robert de Vries <r.devries@robbytu.net>
2014-10-01 18:02:10 +0200

    + Implemented a method to include the model corresponding to the urldef + Moved view rendering to a seperate function (no need to repeat including the base template views now) + Implemented the listObjects view controller

--- app/controllers/ObjectController.php
+++ app/controllers/ObjectController.php
@@ -2,7 +2,7 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * ObjectController
+ * ObjectController.php
  * Hanzecontact
  *
  * Controller that will handle requests made to the urldef's for...
@@ -19,10 +19,47 @@
  */
 
 class ObjectController extends MvcBaseController {
+    /**
+     * @param $modelArg Model from the urldef
+     * @return mixed Specified model object
+     */
+    function loadCorrespondingModel($modelArg) {
+        return $this->loadModel(ucfirst($modelArg) . 'Model');
+    }
 
+    /**
+     * Renders the base template views with a main view
+     * @param $view Name of the main view to render
+     */
+    function renderBaseTemplateWithView($view) {
+        $this->renderView("base/header");
+        $this->renderView($view);
+        $this->renderView("base/footer");
+    }
+
+    /**
+     * Lists the objects in the database
+     * @param $args Arguments passed on from the urldef
+     */
     public function listObjects($args) {
-        echo "List objects<br />";
-        var_dump($args);
+        // Load the model corresponding to the arguments passed on
+        // from the urldef
+        $model = $this->loadCorrespondingModel($args['model']);
+
+        // Set up all data variables
+        $this->data['title'] = $model->friendlyNamePlural;
+
+        $this->data['singleObjectName'] = $model->friendlyNameSingle;
+        $this->data['tableColumns'] = $model->tableColumns;
+        $this->data['tableObjects'] = $model->allObjects();
+
+        // Add urldefs
+        $this->data['addUrl'] = "{$this->MvcInstance->appBaseUrl}/{$args['model']}/new";
+        $this->data['editUrl'] = "{$this->MvcInstance->appBaseUrl}/{$args['model']}/%d/edit";
+        $this->data['removeUrl'] = "{$this->MvcInstance->appBaseUrl}/{$args['model']}/%d/remove";
+
+        // Render the views
+        $this->renderBaseTemplateWithView("object_list");
     }
 
     public function newObject($args) {
@@ -39,5 +76,4 @@ class ObjectController extends MvcBaseController {
         echo "Remove object<br />";
         var_dump($args);
     }
-
 }
