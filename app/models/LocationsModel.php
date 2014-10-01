7a361f8aacdd9757b04f11671bde8b972c12b892
Robert de Vries <r.devries@robbytu.net>
2014-10-01 18:12:23 +0200

    + Implemented LocationsModel

--- app/models/LocationsModel.php
+++ app/models/LocationsModel.php
@@ -15,5 +15,33 @@
  */
 
 class LocationsModel extends MvcBaseModel {
+    /**
+     * @var string Database table name for this model
+     */
+    protected $tableName = "locations";
 
+    /**
+     * @var string Database table primary key field name for this model
+     */
+    protected $tablePrimaryKeyField = "LocationID";
+
+    /**
+     * @var array Columns that need to be written to the screen. Used by the view.
+     */
+    public $tableColumns = [
+        "Adres" => "StreetAddress",
+        "Postcode" => "PostalCode",
+        "Stad" => "City",
+        "Provincie" => "StateProvince",
+        "Land ID" => "CountryID"];
+
+    /**
+     * @var string Friendly name (plural) of the model. Used by the view.
+     */
+    public $friendlyNamePlural = "Locaties";
+
+    /**
+     * @var string Friendly name (single) of the model. Used by the view.
+     */
+    public $friendlyNameSingle = "Locatie";
 }
