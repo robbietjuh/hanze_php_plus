84ad48bca444e54a9301879634d547c9f24d9c36
Robert de Vries <r.devries@robbytu.net>
2014-10-01 20:48:25 +0200

    + Implemented a method to handle the pictures uploaded to the Employee database

--- app/models/EmployeesModel.php
+++ app/models/EmployeesModel.php
@@ -50,4 +50,20 @@ class EmployeesModel extends MvcBaseModel {
      * @var string Friendly name (single) of the model. Used by the view.
      */
     public $friendlyNameSingle = "Medewerker";
+
+    /**
+     * Handle the picture being uploaded when adding or editing an Employee object
+     * @return bool|string Returns false when failed, otherwise the filename of the picture
+     */
+    public function handlePictureUpload() {
+        if(!isset($_FILES["Picture"])) return false;
+
+        // Create a md5 hash of the filename to avoid file name collisions
+        $extension = substr(strrchr($_FILES["Picture"]["name"], '.'), 1);
+        $name = md5(time() . $_FILES["Picture"]["name"]) . "." . $extension;
+
+        // Move the file to the appropriate directory
+        move_uploaded_file($_FILES["Picture"]["tmp_name"], "pictures/$name");
+        return $name;
+    }
 }
