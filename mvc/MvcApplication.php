be10e73086ffa7456d8036d883994e482e40b6b2
Robert de Vries <r.devries@robbytu.net>
2014-10-01 16:20:36 +0200

    + Added a base MvcApplication class

---
+++ mvc/MvcApplication.php
@@ -0,0 +1,60 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * MvcApplication.php
+ * Hanzecontact
+ *
+ * This file contains the main class for the MVC framework, including the bootstrapper,
+ * database connectors and url dispatcher.
+ *
+ * @author     R. de Vries <r.devries@robbytu.net>
+ * @version    1.0.0
+ * @date       01/10/14
+ * @copyright  2014 RobbytuProjects
+ * @license    MIT License
+ */
+
+class MvcApplication {
+    /**
+     * @var string Name of the application
+     */
+    public $appName;
+
+    /**
+     * @var string Version of the application
+     */
+    public $appVersion;
+
+    /**
+     * @var string Base URL of the application, could come in handy when used in combination with Apache's mod_user
+     */
+    public $appBaseUrl;
+
+    /**
+     * @var bool Wether or not the enable detailed debug information
+     */
+    public $debug = false;
+
+    /**
+     * @var array Database connection configuration.
+     *
+     * Should at least contain:
+     *      - Host
+     *      - Username
+     *      - Password
+     *      - Database
+     */
+    protected $database;
+
+    /**
+     * @var array Configured URLs for the application.
+     *
+     * Should be in a format as follows:
+     *      '${Regex URL matcher}' => ['${Controller}', '${Function}']
+     *
+     * The controllername should match the filename and classname of the controller
+     * you want to dispatch to. The functionname is called on the controller.
+     */
+    protected $urls;
+}
