d71383319e1545fb8ede14f9d3fb1fb30d780646
Robert de Vries <r.devries@robbytu.net>
2014-10-01 16:08:01 +0200

    + Added htaccess to rewrite all URLs into index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * index.php
+ * Hanzecontact
+ *
+ * Main entry point of the application
+ *
+ * @author     R. de Vries <r.devries@robbytu.net>
+ * @version    1.0.0
+ * @date       01/10/14
+ * @copyright  2014 RobbytuProjects
+ * @license    MIT License
+ */
+
+var_dump($_GET);
